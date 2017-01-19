f0bfa77e27a1bc47de01d903548dae899709dfd3
NikitaPokryshko <pokryshko.n@gmail.com>
2017-01-19 20:33:13 +0300

    ddw

---
+++ app/_services/site.creation.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {Http, Headers, Response, RequestOptions} from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map'
+import {WebSite} from "../user.page/website";
+
+
+@Injectable()
+export class SiteCreationService {
+
+    currentSiteId: number;
+
+    public token: string;
+    private creationUrl = 'http://localhost:8080/createsite';
+
+    constructor(private http: Http) {
+        // set token if saved in local storage
+
+
+    }
+    createSite(site : WebSite){ debugger;
+         let currentUser  = localStorage.getItem('currentUser');
+
+         this.token = JSON.parse(currentUser).token;
+         console.log(this.token);
+
+         let headers = new Headers(
+                {'Content-Type': 'application/json;charset=utf-8',
+                'Accept' : 'application/json;charset=utf-8',
+                'X-AUTH-TOKEN' : this.token});
+         let options = new RequestOptions({headers: headers});
+         let body = JSON.stringify(site);
+
+         return this.http.post(this.creationUrl, body, options)
+             .map((response: Response)=> {
+                let siteId = response.json() && response.json().siteId;
+             })
+    }
+
+
+}
